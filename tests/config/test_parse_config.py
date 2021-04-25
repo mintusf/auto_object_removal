aa44fae99704422e490927ea24c76b02c897ef47
fmintus <fmintus@ridge-i.com>
2021-04-25 18:39:36 +0900

    Add config parsing and unittest

---
+++ tests/config/test_parse_config.py
@@ -0,0 +1,12 @@
+import os
+import pytest
+
+from utils.load import parse_config
+
+def test_parse_config():
+    default_config_path = os.path.join("config","default.yaml")
+    default_config = parse_config(default_config_path)
+
+    assert default_config['input_type'] in ['image', 'video']
+    assert isinstance(default_config['segmentation_model'], str)
+    assert isinstance(default_config['max_instances'], int) 
