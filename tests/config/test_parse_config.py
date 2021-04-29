ad1f7295f2f336c95789f5e39cec43378b59f4ea
fmintus <fmintus@ridge-i.com>
2021-04-29 13:04:16 +0900

    Fix config unittest

--- tests/config/test_parse_config.py
+++ tests/config/test_parse_config.py
@@ -8,5 +8,5 @@ def test_parse_config():
     default_config = parse_config(default_config_path)
 
     assert default_config['input_type'] in ['image', 'video']
-    assert isinstance(default_config['segmentation_model'], str)
+    assert isinstance(default_config['semantic_segmentation_cfg'], list)
     assert isinstance(default_config['max_instances'], int) 
