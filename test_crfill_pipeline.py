da49ce8d864c228a180f00d6293487244ed58592
fmintus <fmintus@ridge-i.com>
2021-05-03 14:11:55 +0900

    Add crfill unittest

---
+++ test_crfill_pipeline.py
@@ -0,0 +1,31 @@
+import os
+
+# import pytest
+import cv2
+import numpy as np
+
+from models.segmentation.segmentor import Segmentor
+from models.inpainting import CRFillModel
+
+def test_crfill_pipeline():
+    config_path = os.path.join("config", "default.yaml")
+    segmentor = Segmentor(config_path)
+    inpainter = CRFillModel(config_path)
+
+    input_image = cv2.imread(os.path.join("samples", "images", "dog_bicycle_car.jpg"), cv2.IMREAD_COLOR)
+
+    model_output = segmentor.predict_mask(input_image)
+
+    mask_dog = segmentor.get_mask_sem(model_output, "dog")
+    mask_bicycle = segmentor.get_mask_sem(model_output, "bicycle")
+    mask_car = segmentor.get_mask_sem(model_output, "car")
+
+    for i, mask in enumerate([mask_dog, mask_bicycle, mask_car]):
+        inpainted_result = inpainter.inpaint(input_image, mask)
+
+        cv2.imwrite(f'{i}_Mask.png',mask)
+        cv2.imwrite(f'{i}_Image.png',input_image)
+        cv2.imwrite(f'{i}_Inpaint.png',inpainted_result)
+
+
+test_crfill_pipeline()
